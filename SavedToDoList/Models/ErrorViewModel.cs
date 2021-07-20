9737dc6a1fb5e7695de953d2e09d2d005a8354fb
MarijaTV <marijatv@gmail.com>
2021-07-20 18:53:48 +0300

    Add project files.

---
+++ SavedToDoList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SavedToDoList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
